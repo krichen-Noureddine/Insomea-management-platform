91357be8ee0e6870a19090de2e5d69aea95ffed0
krichen-Noureddine <noureddine.krichen@esprit.tn>
2024-07-22 21:55:07 +0100

    fix build error

--- pages/mo365/Subscriptions/[id].jsx
+++ pages/mo365/Subscriptions/[id].jsx
@@ -1,44 +1,38 @@
-import { useRouter } from 'next/router';
-import React, { useEffect, useState } from 'react';
+// pages/mo365/subscriptions/[id].jsx
+import React from 'react';
 import Mo365Details from '@/components/mo365Details';
 
-const SubscriptionDetailPage = () => {
-    const router = useRouter();
-    const { id } = router.query;
-    const [subscription, setSubscription] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        if (id) {
-            const fetchSubscription = async () => {
-                try {
-                    const response = await fetch(`/api/mo365/${id}`);
-                    if (!response.ok) {
-                        throw new Error('Failed to fetch subscription details');
-                    }
-                    const data = await response.json();
-                    setSubscription(data);
-                } catch (error) {
-                    setError(error.message);
-                } finally {
-                    setLoading(false);
-                }
-            };
-
-            fetchSubscription();
-        }
-    }, [id]);
-
-    if (loading) {
-        return <div>Loading...</div>;
-    }
-
+const SubscriptionDetailPage = ({ subscription, error }) => {
     if (error) {
         return <div>Error: {error}</div>;
     }
 
     return subscription ? <Mo365Details subscription={subscription} /> : <div>No subscription found</div>;
 };
+
+export async function getServerSideProps({ params }) {
+    const { id } = params;
+
+    try {
+        const response = await fetch(`http://localhost:3000/api/mo365/${id}`);
+        if (!response.ok) {
+            throw new Error('Failed to fetch subscription details');
+        }
+        const data = await response.json();
+console.log(data);
+        return {
+            props: {
+                subscription: data,
+            },
+        };
+    } catch (error) {
+        return {
+            props: {
+                subscription: null,
+                error: error.message,
+            },
+        };
+    }
+}
 
 export default SubscriptionDetailPage;
